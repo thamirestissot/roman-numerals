91e7f87b1785e26d16d164f7660e44c315a18cb5
thamirestissot <thamirestissot3@gmail.com>
2020-05-20 17:15:02 -0300

    refactoring

--- src/main/java/com/github/thamirestissot/romannumerals/app/Conversor.java
+++ src/main/java/com/github/thamirestissot/romannumerals/app/Conversor.java
@@ -4,20 +4,41 @@ public class Conversor {
 
     public static int toInteger(String romanNumber) {
         int decimalNumber = 0;
+        romanNumber=romanNumber.toUpperCase();
         for (int i = 0; i <= romanNumber.length(); i++) {
-            if (romanNumber.toLowerCase().startsWith("i", i)) {
-                if (romanNumber.toLowerCase().startsWith("v", i + 1)) {
+            if (romanNumber.startsWith("I", i)) {
+                if (romanNumber.startsWith("V", i + 1)) {
                     decimalNumber += 4;
                     i++;
-                } else if (romanNumber.toLowerCase().startsWith("x", i + 1)) {
+                } else if (romanNumber.startsWith("X", i + 1)) {
                     decimalNumber += 9;
+                    i++;
+                } else if (romanNumber.startsWith("L", i + 1)) {
+                    decimalNumber += 49;
+                    i++;
+                } else if (romanNumber.startsWith("C", i + 1)) {
+                    decimalNumber += 99;
+                    i++;
+                } else if (romanNumber.startsWith("D", i + 1)) {
+                    decimalNumber += 499;
+                    i++;
+                } else if (romanNumber.startsWith("M", i + 1)) {
+                    decimalNumber += 999;
                     i++;
                 } else
                     decimalNumber += 1;
-            } else if (romanNumber.toLowerCase().startsWith("v", i))
+            } else if (romanNumber.startsWith("V", i))
                 decimalNumber += 5;
-            else if (romanNumber.toLowerCase().startsWith("x", i))
+            else if (romanNumber.startsWith("X", i))
                 decimalNumber += 10;
+            else if (romanNumber.startsWith("L", i))
+                decimalNumber += 50;
+            else if (romanNumber.startsWith("C", i))
+                decimalNumber += 100;
+            else if (romanNumber.startsWith("D", i))
+                decimalNumber += 500;
+            else if (romanNumber.startsWith("M", i))
+                decimalNumber += 1000;
         }
         return decimalNumber;
     }
@@ -26,6 +47,38 @@ public class Conversor {
         String numeroRomano = new String();
         numeroRomano = "";
         do {
+            while (numeroDecimal >= 1000) {
+                numeroRomano += "M";
+                numeroDecimal -= 1000;
+            }
+            while (numeroDecimal >= 999) {
+                numeroRomano += "IM";
+                numeroDecimal -= 999;
+            }
+            while (numeroDecimal >= 500) {
+                numeroRomano += "D";
+                numeroDecimal -= 500;
+            }
+            while (numeroDecimal >= 499) {
+                numeroRomano += "ID";
+                numeroDecimal -= 499;
+            }
+            while (numeroDecimal >= 100) {
+                numeroRomano += "C";
+                numeroDecimal -= 100;
+            }
+            while (numeroDecimal >= 99) {
+                numeroRomano += "IC";
+                numeroDecimal -= 99;
+            }
+            while (numeroDecimal >= 50) {
+                numeroRomano += "L";
+                numeroDecimal -= 50;
+            }
+            while (numeroDecimal >= 49) {
+                numeroRomano += "IL";
+                numeroDecimal -= 49;
+            }
             while (numeroDecimal >= 10) {
                 numeroRomano += "X";
                 numeroDecimal -= 10;
--- src/test/java/com/github/thamirestissot/romannumerals/app/ConversorTest.java
+++ src/test/java/com/github/thamirestissot/romannumerals/app/ConversorTest.java
@@ -22,6 +22,22 @@ public class ConversorTest {
         Assert.assertEquals(Conversor.toInteger("Xvii"),17);
     }
     @Test
+    public void checkConvertRomansToDecimal49() {
+        Assert.assertEquals(Conversor.toInteger("Il"),49);
+    }
+    @Test
+    public void checkConvertRomansToDecimal99() {
+        Assert.assertEquals(Conversor.toInteger("ic"),99);
+    }
+    @Test
+    public void checkConvertRomansToDecimal499() {
+        Assert.assertEquals(Conversor.toInteger("ID"),499);
+    }
+    @Test
+    public void checkConvertRomansToDecimal999() {
+        Assert.assertEquals(Conversor.toInteger("im"),999);
+    }
+    @Test
     public void checkConvertDecimalToRoman19() {
         Assert.assertEquals(Conversor.toRoman(19),"XIX");
     }
@@ -36,5 +52,21 @@ public class ConversorTest {
     @Test
     public void checkConvertDecimalToRoman17() {
         Assert.assertEquals(Conversor.toRoman(17),"XVII");
+    }
+    @Test
+    public void checkConvertDecimalToRoman49() {
+        Assert.assertEquals(Conversor.toRoman(49),"IL");
+    }
+    @Test
+    public void checkConvertDecimalToRoman99() {
+        Assert.assertEquals(Conversor.toRoman(99),"IC");
+    }
+    @Test
+    public void checkConvertDecimalToRoman499() {
+        Assert.assertEquals(Conversor.toRoman(499),"ID");
+    }
+    @Test
+    public void checkConvertDecimalToRoman999() {
+        Assert.assertEquals(Conversor.toRoman(999),"IM");
     }
 }
